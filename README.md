c3ee5622a71cd6d438ff04a09abf93119f113944
Emmanuel Cortes <ecortes016@west-mec.org>
2019-10-10 15:23:11 -0700

    Fetched Marvel API
    Fetched the Marvel API with my private and public key but I am not sure if I want to continue using it.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ExpressAPI
 ## Project Objectives - [ ] Students will create a server with Express - [ ] Students will use a template engine to create 5 dynamic pages. - [ ] Students will use **fetch** on the **server** and pass the data into the view engine. - [ ] Students will publish a pre-production server to heroku, glitch, AWS, digital ocean, etc .
+
+
+<!-- 
+Marvel Keys
+
+Public: df7cdc55dad592b8c46b213093e9d349
+
+Private: d5708e5cf4c6e72db54f31cb40a89c457af5fa8f
+
+Hash: 73fc3e8eb94b68b934f22ccf02998163
+
+
+1d5708e5cf4c6e72db54f31cb40a89c457af5fa8fdf7cdc55dad592b8c46b213093e9d349
+ -->
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 const hbs = require('express-handlebars');
 const path = require('path');
 
-app.engine('handlebars', hbs({ 
-    defaultLayout: 'main', 
-    layoutsDir: path.join(__dirname, 'views/layouts') 
+app.engine('handlebars', hbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views/layouts')
 }));
 
 app.set('view engine', 'handlebars');
@@ -13,25 +14,62 @@ app.set('view engine', 'handlebars');
 // Routing
 app.get("/", (req, res) => {
     // res.render("index", { layout: false });
-    res.render('index', { 
-        title: 'Home Page', 
+    res.render('index', {
+        title: 'Home Page',
         name: 'Emmanuel Cortes',
         age: 17,
-        isDisplayedName: false
+        isDisplayName: true,
+        isAgeEnable: true
     });
 
-  });
+});
 
 
 app.get("/about", (req, res) => {
     // res.render("about", { layout: false });
-    res.render('about', { 
+    res.render('about', {
         title: "About",
-        description: 'lorem ipsum lorem ipsum' 
+        description: 'lorem ipsum lorem ipsum'
     });
-  });
+});
 
+
+app.get("/dashboard", (req, res) => {
+    res.render('dashboard', {
+        isListEnabled: true
+    })
+})
+
+
+app.get("/each/helper", (req, res) => {
+    res.render('contact', {
+        people: [
+            "James",
+            "Peter",
+            "Samantha",
+            "Marissa"
+        ],
+        user: {
+            username: 'Shroud',
+            age: 25,
+            phone: 1234567
+        },
+        lists: [
+            {
+                items: ['Mango', 'Banana', 'Pineapple']
+            },
+
+            {
+                items: ['Potato', 'Manioc', 'Avocado']
+            }
+        ]
+    });
+})
 
 app.listen(8080, () => {
     console.log('Server is starting at port ', 8080);
 });
+
+fetch('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=df7cdc55dad592b8c46b213093e9d349&hash=73fc3e8eb94b68b934f22ccf02998163')
+    .then(res => res.text())
+    .then(body => console.log(body));
